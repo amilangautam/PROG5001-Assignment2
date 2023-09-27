f5b69501e6c1389808f0b472d04741c1eb42cdd5
Milan Gautam <42036589+amilangautam@users.noreply.github.com>
2023-09-27 16:31:06 +1000

    Marks Threshold applied which is enter by the user
    prints the list of students with total marks less than a certain threshold. The threshold will be entered from the keyboard by the user

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -98,6 +98,17 @@ public class StudentStatistics
             return null;
         }
     }
+    
+    private static double parseDouble(String value) {
+        try {
+            if (!value.isEmpty() && value.matches("[0-9.]+")) {
+                return Double.parseDouble(value);
+            }
+        } catch (NumberFormatException e) {
+            // exception handling
+        }
+        return 0.0; // default value
+    }
     private static void studentTotalMarkCalculation(Student[] students) {
         System.out.println("Student list with their name, student ID, assessment marks and the total mark:");
         for (Student student : students) {
@@ -110,6 +121,21 @@ public class StudentStatistics
                     ", Assignment 2 Mark: " + student.ass2Mark +
                     ", Assignment 3 Mark: " + student.ass3Mark +
                     ", Total Mark: " + totalMark);
+            }
+        }
+    }
+    
+    private static void studentsBelowThreshold(Student[] students, double threshold) {
+        System.out.println("Total marks below  " + threshold + ":");
+        for (Student student : students) {
+            if (student != null) { 
+                // null check
+                double totalMark = student.ass1Mark + student.ass2Mark + student.ass3Mark;
+                if (totalMark < threshold) {
+                    System.out.println("Name: " + student.firstName + " " + student.lastName +
+                        ", Student ID: " + student.studentID +
+                        ", Total Mark: " + totalMark);
+                }
             }
         }
     }
