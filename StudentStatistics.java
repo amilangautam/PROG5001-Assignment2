1ad38a79e0329019edf871b4bf2c0ddcd1cd2bb0
Milan Gautam <42036589+amilangautam@users.noreply.github.com>
2023-09-29 16:45:39 +1000

    top 5 student with the highest & lowest total mark
    prints the top 5 students with the highest total marks and the top 5 students with the lowest total marks.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -8,17 +8,17 @@ import java.util.*;
  */
 class Student {
     //Variable Declaration
-    String lastName;
     String firstName;
+    String lastName;
     String studentID;
     double ass1Mark;
     double ass2Mark;
     double ass3Mark;
     double totalMark;
 
-    public Student(String lastName, String firstName, String studentID, double ass1Mark, double ass2Mark, double ass3Mark) {
-        this.lastName = lastName;
+    public Student(String firstName, String lastName, String studentID, double ass1Mark, double ass2Mark, double ass3Mark) {
         this.firstName = firstName;
+        this.lastName = lastName;
         this.studentID = studentID;
         this.ass1Mark = ass1Mark;
         this.ass2Mark = ass2Mark;
@@ -135,6 +135,23 @@ public class StudentStatistics
                     System.out.println("Name: " + student.firstName + " " + student.lastName +
                         ", Student ID: " + student.studentID +
                         ", Total Mark: " + totalMark);
+                }
+            }
+        }
+    }
+    private static void sortStudentUsingBubbleSort(Student[] students, boolean ascending) {
+        int num = students.length;
+        for (int j = 0; j < num - 1; j++) {
+            for (int k = 0; k < num - j - 1; k++) {
+                if (students[k] != null && students[k + 1] != null) {
+                    double mark1 = students[k].totalMark;
+                    double mark2 = students[k + 1].totalMark;
+
+                    if ((ascending && mark1 > mark2) || (!ascending && mark1 < mark2)) {
+                        Student temp = students[k];
+                        students[k] = students[k + 1];
+                        students[k + 1] = temp;
+                    }
                 }
             }
         }
